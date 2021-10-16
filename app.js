d62ed1dd0a6c9cc2880c2ca032692e0aa06a0123
Keller Beckstead <kellerjbeckstead@gmail.com>
2021-10-16 16:49:00 -0600

    last commit for this

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-        const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+        const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
         fileStream.on('open',() => {
-            fileStream.pipe()
+            fileStream.pipe(res)
         })
         fileStream.on('error', (err) => {
             res.end(err)
